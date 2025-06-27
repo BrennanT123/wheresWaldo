0887b946691bd084d55221c11e9f305b7acbb4cf
BrennanT123 <113391949+BrennanT123@users.noreply.github.com>
2025-06-27 17:54:32 -0400

    debug cookies

--- backend/routes/gameFunctionsRouter.js
+++ backend/routes/gameFunctionsRouter.js
@@ -1,25 +1,26 @@
-import express from "express"
-import * as gameCtrl from "../controllers/gameFunctionsController.js"
+import express from "express";
+import * as gameCtrl from "../controllers/gameFunctionsController.js";
 
 const gameRouter = express.Router();
 
 gameRouter.get("/leaderboard", gameCtrl.getLeaderboard);
 
-gameRouter.post("/evaluateGuess",gameCtrl.postEvaluateGuess);
+gameRouter.post("/evaluateGuess", gameCtrl.postEvaluateGuess);
 gameRouter.get("/setup", gameCtrl.getStartup);
-gameRouter.get("/getScenes",gameCtrl.getScenes);
-gameRouter.post("/postScene",gameCtrl.postScene);
-gameRouter.post("/postCurrentGame",gameCtrl.postCurrentGame);
+gameRouter.get("/getScenes", gameCtrl.getScenes);
+gameRouter.post("/postScene", gameCtrl.postScene);
+gameRouter.post("/postCurrentGame", gameCtrl.postCurrentGame);
 
-gameRouter.post("/startGame",gameCtrl.postStartGame);
-gameRouter.post("/endGame",gameCtrl.postEndGame);
-gameRouter.post("/updateLeaderboard",gameCtrl.postUpdateLeaderBoard);
-gameRouter.delete("/deleteCurrentGame",gameCtrl.deleteCurrentGame);
-gameRouter.get("/checkEndgame",gameCtrl.getCheckEndgame);
+gameRouter.post("/startGame", gameCtrl.postStartGame);
+gameRouter.post("/endGame", gameCtrl.postEndGame);
+gameRouter.post("/updateLeaderboard", gameCtrl.postUpdateLeaderBoard);
+gameRouter.delete("/deleteCurrentGame", gameCtrl.deleteCurrentGame);
+gameRouter.get("/checkEndgame", gameCtrl.getCheckEndgame);
 gameRouter.get("/api/test-session", (req, res) => {
   if (!req.session.views) req.session.views = 0;
   req.session.views++;
-  res.send(`You have visited this page ${req.session.views} times`);
+  req.session.test = "force save";
+  res.send("Session cookie should be set now");
 });
 
 gameRouter.get("/test-cookie", (req, res) => {
@@ -30,8 +31,5 @@ gameRouter.get("/test-cookie", (req, res) => {
   });
   res.send("Cookie sent");
 });
-
-
-
 
 export default gameRouter;
